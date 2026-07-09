074272ec5c03b42c7fa8257ce2aa2dd88d518778
pramaditha35-star <pramaditha35@gmail.com>
2026-07-09 22:49:35 +0800

    Delete javaswing directory

--- javaswing/compile_and_run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Build and Run Ticket Sales Information System
-echo Launching build process via PowerShell...
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0run.ps1"
-if %ERRORLEVEL% neq 0 (
-    echo.
-    echo Script encountered an error.
-)
-pause
